ffb46a200f21532940696fa697b08e2470cbd7da
Amy McTigue <amy.mctigue@ovoenergy.com>
2023-09-06 16:03:04 +0100

    Added Nav Process Run test

--- Automation_tests/Navigation_GT.spec.js
+++ Automation_tests/Navigation_GT.spec.js
@@ -1,20 +1,36 @@
-// Smoke test 2: Navigation 
+// Smoke test 2: Navigation
 import { test, expect } from "@playwright/test";
-import 'dotenv/config';
- 
+import "dotenv/config";
+
 const userName = process.env.USER_NAME;
 const rightPassword = process.env.RIGHT_PASSWORD;
 
-test('Navigation - File', async ({page}) => {
-    await page.goto("http://gt-preprod:8081/MOD/UI/LOGIN?RelayState=");
-    await page.getByLabel("User Name").fill(userName);
-    await page.getByLabel("Password").fill(rightPassword);
-    await page.getByRole("button", { name: "Login" }).click();
-    await expect(page).toHaveTitle("Gentrack Launch Pad - Ovo Energy - PREPROD");
-    await page.locator('[id="GenUI\\.MenuControl_3_input"]').fill("err");
-    await page.waitForTimeout(1000); 
-    await page.waitForSelector('text="View Error Events"');
-    await page.click('text="View Error Events"');
-    await expect(page).toHaveTitle("Error Events - Ovo Energy - PREPROD");
-  })
-  
+test("Navigation - File", async ({ page }) => {
+  // Log In to preprod
+  await page.goto("http://gt-preprod:8081/MOD/UI/LOGIN?RelayState=");
+  await page.getByLabel("User Name").fill(userName);
+  await page.getByLabel("Password").fill(rightPassword);
+  await page.getByRole("button", { name: "Login" }).click();
+  await expect(page).toHaveTitle("Gentrack Launch Pad - Ovo Energy - PREPROD");
+  // Navigate to error events page and check the title
+  await page.locator('[id="GenUI\\.MenuControl_3_input"]').fill("error");
+  await page.getByText("View Error Events").click();
+  await expect(page).toHaveTitle("Error Events - Ovo Energy - PREPROD");
+});
+
+
+test('Navigation - Run IFM Process', async ({ page }) => {
+  // Log in to preprod
+  await page.goto("http://gt-preprod:8081/MOD/UI/LOGIN?RelayState=");
+  await page.getByLabel("User Name").fill(userName);
+  await page.getByLabel("Password").fill(rightPassword);
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page).toHaveTitle("Gentrack Launch Pad - Ovo Energy - PREPROD");
+  // Navigate to File Interface Manager
+  await page.locator('[id="GenUI\\.MenuControl_3_input"]').fill('file');
+  await page.getByText('File Interface Manager').click();
+  await expect(page).toHaveTitle("Interface File Manager - Ovo Energy - PREPROD")
+  // Run Load process
+  await page.getByText('B1UKMKT|LOAD', { exact: true }).dblclick();
+  await page.getByRole('button', { name: 'Next' }).click();
+});
--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { defineConfig, devices } = require('@playwright/test');
+const { defineConfig, devices } = require("@playwright/test");
 
 /**
  * Read environment variables from file.
@@ -11,37 +11,44 @@ const { defineConfig, devices } = require('@playwright/test');
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-  testDir: './Automation_tests',
+  testDir: "./Automation_tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: "http://gt-preprod:8081/MOD/UI/LOGIN?RelayState=",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
+
+    screenshot: "only-on-failure",
+    //allows the video to be recorded and can be viewed in slowmotion
+    video: "on",
+    launchOptions: {
+      slowMo: 1000, // slow down by 1000ms
+    },
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
 
     // {
     //   name: 'webkit',
@@ -76,4 +83,3 @@ module.exports = defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
