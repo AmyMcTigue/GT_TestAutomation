f92db12436d5d631723b2618f1c0f9600b3590bf
Amy McTigue <amy.mctigue@ovoenergy.com>
2023-09-14 16:13:49 +0100

    Added var and if function to check if the process is running

--- Automation_tests/ProcessRun_GT.spec.js
+++ Automation_tests/ProcessRun_GT.spec.js
@@ -4,6 +4,8 @@ import 'dotenv/config';
 const userName = process.env.USER_NAME;
 const rightPassword = process.env.RIGHT_PASSWORD;
 
+const processName = "MIS_BATCHLETTER"; 
+
 test.describe('All test. Login Group', () => {
     test.beforeEach(async ({page}) => {
         await page.goto('http://gt-preprod:8081/MOD/UI/LOGIN?RelayState=');
@@ -14,7 +16,7 @@ test.describe('All test. Login Group', () => {
         await expect(page).toHaveTitle('Gentrack Launch Pad - Ovo Energy - PREPROD');
     });
 
-    test('Turn on process schedules', async ({page}) => {
+    test('Turn on/off process schedules', async ({page}) => {
         await page.locator('[id="GenUI\\.MenuControl_3_input"]').click();
         await page.keyboard.insertText("process");
         //Handler event is for keydown,keyup only. Search will not work without this.
@@ -22,7 +24,29 @@ test.describe('All test. Login Group', () => {
         await page.getByText("Process Schedule Configuration").click();
         await expect(page).toHaveTitle("Process Schedule Configuration - Ovo Energy - PREPROD");
         await page.locator('xpath=//html/body/div[3]/div[3]/div/div[1]/div/div[2]/div/div/div[2]/div/table/tbody/tr[1]/td[1]/div/input').click();
-        // TODO: Finish off test - add var to input schedule name etc 
+        await page.keyboard.insertText(processName); 
+        await page.getByRole('button', {name: 'View grid'}).click(); 
+        await page.getByText(processName).dblclick(); 
+        await expect(page).toHaveTitle("Maintain Process Schedule - Ovo Energy - PREPROD"); 
+        await page.getByRole('link', { name: 'Schedule Run Status' }).click(); 
+        await expect(page).toHaveTitle("Change Schedule Run Status - Ovo Energy - PREPROD"); 
+        
+        // TODO: Fix this ridiculous mess - Unsure why the if statement is not working. 
+        // To turn on process
+        if (expect(page.getByText('Pause Flag'))) {
+             // To turn on schedule use the below line
+                await page.locator('div').filter({ hasText: /^Reset schedule to run normally$/ }).click();
+                await page.getByRole("button", { name: "Next" }).click();
+                await expect(page).toHaveTitle("Maintain Process Schedule - Ovo Energy - PREPROD");
+                } else {
+                    console.log('Process Schedule already enabled');;
+            }
+        
+
+        // // To turn off schedule use the below line
+        // await page.getByText('Manually suspend schedule from running').click();
+        // await page.getByRole("button", { name: "Next" }).click();
+        // await expect(page).toHaveTitle("Maintain Process Schedule - Ovo Energy - PREPROD"); 
     });
 
 }); 
