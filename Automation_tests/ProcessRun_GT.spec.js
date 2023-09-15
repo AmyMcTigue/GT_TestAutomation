ef2454905d69d6b00d61755d20b39d809695d87f
Amy McTigue <amy.mctigue@ovoenergy.com>
2023-09-15 16:36:00 +0100

    Worked on if statement - Still not working as expected - added error response

--- Automation_tests/ProcessRun_GT.spec.js
+++ Automation_tests/ProcessRun_GT.spec.js
@@ -6,7 +6,6 @@ const rightPassword = process.env.RIGHT_PASSWORD;
 
 const processName = "MIS_BATCHLETTER"; 
 
-test.describe('All test. Login Group', () => {
     test.beforeEach(async ({page}) => {
         await page.goto('http://gt-preprod:8081/MOD/UI/LOGIN?RelayState=');
         await page.getByLabel('User Name').fill(userName);
@@ -19,7 +18,7 @@ test.describe('All test. Login Group', () => {
     test('Turn on/off process schedules', async ({page}) => {
         await page.locator('[id="GenUI\\.MenuControl_3_input"]').click();
         await page.keyboard.insertText("process");
-        //Handler event is for keydown,keyup only. Search will not work without this.
+        // Handler event is for keydown,keyup only. Search will not work without this.
         await page.keyboard.press("ArrowDown");
         await page.getByText("Process Schedule Configuration").click();
         await expect(page).toHaveTitle("Process Schedule Configuration - Ovo Energy - PREPROD");
@@ -28,25 +27,27 @@ test.describe('All test. Login Group', () => {
         await page.getByRole('button', {name: 'View grid'}).click(); 
         await page.getByText(processName).dblclick(); 
         await expect(page).toHaveTitle("Maintain Process Schedule - Ovo Energy - PREPROD"); 
-        await page.getByRole('link', { name: 'Schedule Run Status' }).click(); 
-        await expect(page).toHaveTitle("Change Schedule Run Status - Ovo Energy - PREPROD"); 
         
-        // TODO: Fix this ridiculous mess - Unsure why the if statement is not working. 
+        // TODO: If statement still not working as expected. Error - Timed out 5000ms waiting for expect(received).toBeVisible()
+        // Call log:
+        // - expect.toBeVisible with timeout 5000ms
+        // - waiting for getByRole('cell', { name: 'Pause Flag' })  
+
+
         // To turn on process
-        if (expect(page.getByText('Pause Flag'))) {
-             // To turn on schedule use the below line
+        if (await expect(page.getByRole('cell', { name: 'Pause Flag' })).toBeVisible()) {
+                // To turn on schedule use the below line
+                await page.getByRole('link', { name: 'Schedule Run Status' }).click(); 
+                await expect(page).toHaveTitle("Change Schedule Run Status - Ovo Energy - PREPROD"); 
                 await page.locator('div').filter({ hasText: /^Reset schedule to run normally$/ }).click();
                 await page.getByRole("button", { name: "Next" }).click();
                 await expect(page).toHaveTitle("Maintain Process Schedule - Ovo Energy - PREPROD");
-                } else {
+                } else if (await expect(page.getByRole('cell',{name:'Pause Flag'})).toBeHidden) {
                     console.log('Process Schedule already enabled');;
-            }
-        
-
+                    await expect(page).toHaveTitle("Maintain Process Schedule - Ovo Energy - PREPROD");
+                    }   
         // // To turn off schedule use the below line
         // await page.getByText('Manually suspend schedule from running').click();
         // await page.getByRole("button", { name: "Next" }).click();
         // await expect(page).toHaveTitle("Maintain Process Schedule - Ovo Energy - PREPROD"); 
     });
-
-}); 
