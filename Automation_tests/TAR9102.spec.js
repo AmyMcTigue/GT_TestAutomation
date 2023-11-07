266805d9d5418c12788c958a20f25908139358a0
Amy McTigue <amy.mctigue@ovoenergy.com>
2023-11-07 16:05:20 +0000

    Refactored and added customer search

--- Automation_tests/TAR9102.spec.js
+++ Automation_tests/TAR9102.spec.js
@@ -5,24 +5,13 @@ const userName = process.env.USER_NAME;
 const rightPassword = process.env.RIGHT_PASSWORD;
 
 // Declare test variables 
-const testAcc = `"25"`; 
-const priceChangeDate = '';
-const streamDetails = `ISELECT CONSUMER WITH QCACCT = ${testAcc}`;
-
- /*   test.beforeAll(async ({page}) => {
-        //Log in to GT preprod
-        await page.goto('http://gt-preprod:8081/MOD/UI/LOGIN?RelayState=');
-        await page.getByLabel('User Name').fill(userName);
-        await page.getByLabel('Password').fill(rightPassword);
-        await page.getByRole('button', {name: 'Login'}).click();
-        await page.url('http://gt-preprod:8081/#');
-        await expect(page).toHaveTitle('Gentrack Launch Pad - Ovo Energy - PREPROD');
-    });
-*/
+const testAccount = `2107842`; 
+const streamFormat = (`"`+testAccount+`"`);
+const streamDetails = `ISELECT CONSUMER WITH QCACCT = ${streamFormat}`;
  
-    test('TAR9102 - Run', async ({page}) => {
+test('TAR9102 - Run', async ({page}) => {
        
-          //Log in to GT preprod
+    //Log in to GT preprod
           await page.goto('http://gt-preprod:8081/MOD/UI/LOGIN?RelayState=');
           await page.getByLabel('User Name').fill(userName);
           await page.getByLabel('Password').fill(rightPassword);
@@ -30,8 +19,7 @@ const streamDetails = `ISELECT CONSUMER WITH QCACCT = ${testAcc}`;
           await page.url('http://gt-preprod:8081/#');
           await expect(page).toHaveTitle('Gentrack Launch Pad - Ovo Energy - PREPROD');
 
-
-        // Design Process 
+    // Design Process 
         await page.goto('http://gt-preprod:8081/#REPORTOVERVIEW.SCR??132367836/REPORT.SCR?REPORT=TAR9102?143887791'); 
         await expect(page).toHaveTitle("Process Configuration - Ovo Energy - PREPROD");
         await page.getByRole('link', { name: 'Maintain Process' }).click(); 
@@ -39,9 +27,11 @@ const streamDetails = `ISELECT CONSUMER WITH QCACCT = ${testAcc}`;
         await page.locator('xpath=//html/body/div[3]/div[3]/div/div[1]/div[3]/div[2]/div[1]/div/table/tbody/tr[3]/td[1]/div/textarea').fill('');
         await page.locator('xpath=//html/body/div[3]/div[3]/div/div[1]/div[3]/div[2]/div[1]/div/table/tbody/tr[3]/td[1]/div/textarea').fill(streamDetails);
         await page.getByRole("button", { name: "Next" }).click();
+        await page.waitForTimeout(3000); // waits for 3 seconds
 
-        // Refresh Cache 
-        // TODO - figure out why js function will not work here (possibly need to use page.evaluate??)
+    // Refresh Cache 
+    
+        // TODO: figure out why js function will not work here (possibly need to use page.evaluate??)
         // const refresh = function() {
         //     await page.goto('http://gt-preprod:8081/#REFRESH.WIZ??1146948'); 
         //     await expect(page).toHaveTitle("Refresh Programs and Parameters - Ovo Energy - PREPROD");
@@ -49,8 +39,7 @@ const streamDetails = `ISELECT CONSUMER WITH QCACCT = ${testAcc}`;
         //     await page.getByRole("button", { name: "Next" }).click();
         //     await page.waitForTimeout(3000); // waits for 3 seconds
         // };
-        // refresh();
-        
+        // refresh();    
 
         await page.goto('http://gt-preprod:8081/#REFRESH.WIZ??1146948'); 
         await expect(page).toHaveTitle("Refresh Programs and Parameters - Ovo Energy - PREPROD");
@@ -58,20 +47,25 @@ const streamDetails = `ISELECT CONSUMER WITH QCACCT = ${testAcc}`;
         await page.getByRole("button", { name: "Next" }).click();
         await page.waitForTimeout(3000); // waits for 3 seconds
 
-        // Run Process 
+    // Run Process 
         // await page.getByRole("button", { name: "Run" });
-        await page.goto('http://gt-preprod:8081/#REPORTOVERVIEW.SCR??132367836')
-        await expect(page).toHaveTitle("Process Overview - Ovo Energy - PREPROD");
-        await page.waitForTimeout(5000); // waits for 5 seconds
-        await page.getByRole('cell', { name: 'Process Code' }).locator('div').dblclick();
-        await page.waitForTimeout(5000); // waits for 5 seconds
-        await page.getByText("TAR9102").click();
-        await page.keyboard.press("Enter");
-        await expect(page).toHaveTitle("Overdue Consumer Contract Report - Ovo Energy - PREPROD"); 
-        await page.getByRole('link', { name: 'Run Process' }).click(); 
-        await expect(page).toHaveTitle("Run Process - Ovo Energy - PREPROD");
+        await page.goto('http://gt-preprod:8081/#REPORTOVERVIEW.SCR??153921761/TAR9102.PROCESS?TAR9102=TAR9102%3ADEFAULT?153920552')
+            await expect(page).toHaveTitle("Overdue Consumer Contract Report - Ovo Energy - PREPROD"); 
+            await page.getByRole('link', { name: 'Run Process' }).click(); 
+            await expect(page).toHaveTitle("Run Process - Ovo Energy - PREPROD");
+            await page.getByRole("button", { name: "Next" }).click();
+            await page.waitForTimeout(10000); // waits for 10 seconds
+            await expect(page).toHaveTitle(" - Ovo Energy - PREPROD");
+
+           
+    // TODO: Split into two tests that should be run sequentally to reduce the testing time 
+    // Go to Customer account 
+        await page.goto('http://gt-preprod:8081/#')
+        await page.locator("xpath=//html/body/div[3]/div[3]/div/div[1]/div[1]/div[2]/div[1]/div/table/tbody/tr[1]/td[1]/div/input").fill(testAccount);
         await page.getByRole("button", { name: "Next" }).click();
-        await page.waitForTimeout(5000); // waits for 5 seconds
-        await expect(page).toHaveTitle("Process Monitor Details - Ovo Energy - PREPROD");
+        // Xpath for first row in the search results - click to select
+        await page.locator("xpath=//html/body/div[3]/div[3]/div/div[1]/div/div[2]/div[1]/div/div[4]/table/tbody/tr/td[1]/span").click(); // Click on the first row
+        await page.keyboard.press("ArrowUp");
+        await page.keyboard.press("Enter");
     });
 
